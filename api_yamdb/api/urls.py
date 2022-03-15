9abce9a7ea13204d67d7b995fa399eb094eb131b
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-03-16 00:17:56 +0300

    добавил роуты и заготовку вьюсетов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,13 +2,22 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import (CategoryViewSet, GenreViewSet,
+                    TitleViewSet, ReviewViewSet, CommentViewSet)
 
 router = routers.DefaultRouter()
 
 router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
 router.register('titles', TitleViewSet)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
