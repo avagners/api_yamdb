d7efd0f1dc0dfb5bb6f048adc6064038798e0c29
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-03-15 23:33:04 +0300

    add_models_review_comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+
+
+class Review(models.Model):
+    """Модель отзывов."""
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'], name='unique_review'
+            )
+        ]
+
+
+class Comment(models.Model):
+    """Модель отзывов."""
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments')
