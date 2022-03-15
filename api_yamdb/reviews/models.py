ae3115617f232d8c81960e3f65107dd9e3e929e2
MelatoZa <so_kat@mail.ru>
2022-03-15 22:31:02 +0300

    Создан файл reviews/models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.slug
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True)
+
+    def __str__(self):
+        return self.slug
