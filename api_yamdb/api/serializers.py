ec01737767bfc0e6521f5008206cab0967d22e81
Dmitry Stelmakhov <jaiz@yandex.ru>
2022-03-16 22:44:37 +0300

    добавлены сериализаторы отзывов и комментариев

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+from email.policy import default
+from rest_framework import serializers, validators
 from users.models import User
+from reviews.models import Review, Comment, Title
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,3 +11,33 @@ class UserSerializer(serializers.ModelSerializer):
             'username', 'email', 'first_name', 'last_name', 'bio', 'role'
         )
         ref_name = 'ReadOnlyUsers'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор модели отзывов."""
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username')
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate_score(self, value):
+        score = [i for i in range(1, 11)]
+        if value not in score:
+            raise serializers.ValidationError(
+                'Оценка должна быть в диапазоне 1-10.'
+            )
+        return value
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор модели комментариев."""
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
