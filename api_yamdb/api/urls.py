ec60a6d121498308a2a3f8edf5449f970d66852b
Alexander Vagner <onemybox@gmail.com>
2022-03-17 13:38:58 +0300

    настроил получение JWT токенов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import (CategoryViewSet, GenreViewSet,
                     TitleViewSet, ReviewViewSet, CommentViewSet,
@@ -23,5 +23,8 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path(
+        'v1/auth/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
 ]
