454936d013c6a2bcc338ab1487c50b55e5d8bb64
Alexander Vagner <vagner_a_a@magnit.ru>
2022-03-20 05:28:40 +0300

    fixed permission IsSuperUser

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -52,4 +52,4 @@ class IsUser(BasePermission):
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-                and request.user.is_superuser)
+                and request.user.is_superuser or request.user.role == 'admin')
