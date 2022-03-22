5c7b8cf64a760c0100517f132a67313fc675fb68
Alexander Vagner <onemybox@gmail.com>
2022-03-22 11:59:35 +0300

    #36: вынес пользователькие роли в отдельную модель, сверил размер полей родительского класса с ТЗ

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,14 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class UserRole(models.TextChoices):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
 
-    USER_ROLE = (
-        ("user", "user"),
-        ("moderator", "moderator"),
-        ("admin", "admin"),
-    )
+
+class User(AbstractUser):
+    """Модель пользователей."""
     email = models.EmailField(max_length=254, unique=True)
     bio = models.TextField(blank=True,)
-    role = models.TextField(choices=USER_ROLE, default='user')
-    confirmation_code = models.CharField(max_length=6, default='000000')
+    role = models.TextField(choices=UserRole.choices, default=UserRole.USER)
+    confirmation_code = models.TextField(default='000000')
