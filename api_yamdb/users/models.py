cd9bd1009808703568a0cd1f587ec26b1c413327
Alexander Vagner <onemybox@gmail.com>
2022-03-25 10:11:24 +0300

    #72 поправил модель User: добавил несколько полей, добавил verbose_name

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
 
@@ -7,15 +8,33 @@ class UserRole:
     MODERATOR = 'moderator'
     ADMIN = 'admin'
     choices = [
-        (USER, 'user'),
-        (ADMIN, 'admin'),
-        (MODERATOR, 'moderator'),
+        (USER, 'USER'),
+        (ADMIN, 'ADMIN'),
+        (MODERATOR, 'MODERATOR'),
     ]
 
 
 class User(AbstractUser):
     """Модель пользователей."""
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+        verbose_name='Username'
+    )
     email = models.EmailField(max_length=254, unique=True)
-    bio = models.TextField(blank=True,)
-    role = models.TextField(choices=UserRole.choices, default=UserRole.USER)
+    first_name = models.TextField(max_length=150, blank=True)
+    last_name = models.TextField(max_length=150, blank=True)
+    bio = models.TextField(blank=True, verbose_name='Биография')
+    role = models.TextField(
+        choices=UserRole.choices,
+        default=UserRole.USER,
+        verbose_name='Пользовательская роль'
+    )
     confirmation_code = models.TextField(default='000000')
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'Пользователи'
